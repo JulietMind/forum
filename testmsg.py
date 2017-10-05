95972578028be0a810e54b18e5031f7a0796f195
JulietMind <32484424+JulietMind@users.noreply.github.com>
2017-10-05 20:50:31 +0200

    Hello Brython

--- testmsg.py
+++ testmsg.py
@@ -1,11 +1,2 @@
-import sockets
-import sys
-
-
-# Creando el socket TCP/IP
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Enlace de socket y puerto
-server_address = ('localhost', 10000)
-print >>sys.stderr, 'empezando a levantar %s puerto %s' % server_address
-sock.bind(server_address)
+from browser import document
+data = document['nombre']
