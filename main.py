07f2e3e60b7c37d35e544ed946ff8173b22c74cd
JulietMind <32484424+JulietMind@users.noreply.github.com>
2017-10-06 01:29:24 +0200

    .kv file name of the App error solved

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-import kivy
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.config import Config
+from kivy.uix.widget import Widget
 
-Config.set('graphics', 'width', 800)
-Config.set('graphics', 'height', 400)
 
-class content01(BoxLayout):
-	None
+class PongGame(Widget):
+    pass
 
-class MainApp(App):
-	title = "Hola Mundo"
-	def build(self):
-		return content01()
+
+class PongApp(App):
+    def build(self):
+        return PongGame()
+
 
 if __name__ == '__main__':
-	MainApp().run()
+    PongApp().run()
+	
