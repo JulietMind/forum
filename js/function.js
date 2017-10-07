6fb5de226996bee5e396f011a47c4453ae4cf1eb
JulietMind <32484424+JulietMind@users.noreply.github.com>
2017-10-07 10:12:48 +0200

    Added toggleClass

---
+++ js/function.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.img-menu').on('click', function(){
+		$('.content-nav ul').toggleClass('show')
+	})
+})
+
+// $(document).ready(function(){
+// 	$('.img-menu').on('click',function(){
+// 		$('header ul').toggleClass('show');
+// 	})
+// })
